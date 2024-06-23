88b996c4380d447c887c209fa9a3e0c472356d85
AdriALV2005 <adrimon7221@gmail.com>
2024-06-23 15:55:47 -0500

    asd

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Inicio from './pages/Inicio/Inicio.jsx';
+
+
+function App() {
+    return (
+        <div>
+      
+            <Inicio />
+        </div>
+
+    )
+}
+
+export default App
